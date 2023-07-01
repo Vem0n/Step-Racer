ee8f8a6f9baa04c498002462df7d230c422c5d24
Vem0n <94292470+Vem0n@users.noreply.github.com>
2023-07-01 17:58:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use((err, req, res, next) => {
   
   job.start();
   job2.start();
+  console.log('Jobs started succesfully');
 
   mongoose.connect(keys.mdbKey).then(result => {
     app.listen(8080);
